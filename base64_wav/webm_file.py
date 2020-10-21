cf2e3e05b3e7d6726ca6048adda09b3c78b0f136
lbinding <lawrence.binding@outlook.com>
2020-10-21 16:08:18 +0100

    Cleaning + bug fix

--- base64_wav/webm_file.py
+++ base64_wav/webm_file.py
@@ -5,16 +5,17 @@ import argparse
 import os
 
 def webm_file(b64_file, output):
-    with open('1.txt', 'r') as file:
+    with open(b64_file, 'r') as file:
         data = file.read()
     decodedData = base64.b64decode(data)
     webmfile = output + ".webm"
     with open(webmfile, 'wb') as file:
         file.write(decodedData)
+    del data
 
 # inputs
 parser = argparse.ArgumentParser()
-parser.add_argument('--b64', dest='b64_file', help='Base 64 file', required=True)
+parser.add_argument('--in', dest='b64_file', help='Base 64 file', required=True)
 parser.add_argument('--out', dest='output', help='Base 64 file', required=True)
 
 args = parser.parse_args()
